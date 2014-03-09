df6ce1a6e767df029c994f65a71364d6e42b34d0
Michael Dym <dymmchl@yahoo.com>
2014-03-08 20:01:54 -0500

    kicking off

---
+++ fizzy.rb
@@ -0,0 +1,12 @@
+#this is really to work on git skills but come back to this file and redo fizz_buzz 
+#exercise
+
+<html5>
+  <body>
+    <ul>
+      <l> donuts </l>
+      <l> peanuts </l>
+      <l> black jelly beans </l>
+
+  </body>
+</html>
